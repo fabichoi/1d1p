7c3bbde835d570d5f59cc29d7a47a019c99e2fcb
fabichoi <fabianus.c@gmail.com>
2020-10-21 22:45:54 +0900

    1d1p 2020.09.28

---
+++ src_43rd_week.py
@@ -0,0 +1,13 @@
+# boj 6186
+
+def solve():
+    r, c = map(int, input().split(' '))
+    ar = []
+    for _ in range(r):
+        ar.append(input())
+
+
+
+
+if __name__ == "__main__":
+    solve()
