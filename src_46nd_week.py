6b038ce41dafb1dec8ed70dccbef20137fd8d21a
fabichoi <fabianus.c@gmail.com>
2020-11-12 23:44:35 +0900

    1d1p 2020.11.12

--- src_46nd_week.py
+++ src_46nd_week.py
@@ -1,7 +1,64 @@
+#boj 14620
+'''
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+if __name__ == "__main__":
+    n = int(input())
+    board = []
+    for i in range(n):
+        board.append(list(map(int, input().split(' '))))
+    nb = [[0 for _ in range(n)] for __ in range(n)]
+    visited = [[0 for _ in range(n)] for __ in range(n)]
+
+    for y in range(1, n - 1):
+        for x in range(1, n - 1):
+            nb[y][x] = board[y][x] + board[y - 1][x] + board[y][x - 1] + board[y][x + 1] + board[y + 1][x]
+
+    res = 987654321
 
 
+    def dfs(x, y, sum, cnt):
+        if visited[y][x] or visited[y - 1][x] or visited[y + 1][x] or visited[y][x + 1] or visited[y][x - 1]:
+            return 0
+        if x < 1 or x >= n - 1 or y < 1 or y >= n - 1:
+            return 0
+
+        if cnt == 2:
+            global res
+            res = min(res, sum + nb[y][x])
+            return 0
+
+        visited[y][x] = 1
+        visited[y - 1][x] = 1
+        visited[y + 1][x] = 1
+        visited[y][x - 1] = 1
+        visited[y][x + 1] = 1
+
+        sum += nb[y][x]
+
+        for yy in range(1, n - 1):
+            for xx in range(1, n - 1):
+                dfs(yy, xx, sum, cnt + 1)
+
+        visited[y][x] = 0
+        visited[y - 1][x] = 0
+        visited[y + 1][x] = 0
+        visited[y][x - 1] = 0
+        visited[y][x + 1] = 0
+
+        sum -= nb[y][x]
+
+        return 0
 
 
+    for y in range(1, n - 1):
+        for x in range(1, n - 1):
+            dfs(x, y, 0, 0)
+
+    print(res)
+'''
 #boj 1058
 
 '''
