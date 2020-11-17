f3a434899c3830a8ae160c1a97ff5aa9f2898149
fabichoi <fabianus.c@gmail.com>
2020-11-17 21:33:28 +0900

    1d1p 2020.11.17

---
+++ src_47th_week.py
@@ -0,0 +1,59 @@
+# boj 11729
+
+if __name__ == "__main__":
+    n = int(input())
+
+
+    def hanoi(n, a, b, c):
+        if n == 1:
+            print(a, c)
+        else:
+            hanoi(n - 1, a, c, b)
+            print(a, c)
+            hanoi(n - 1, b, a, c)
+
+
+    print(2 ** n - 1)
+    hanoi(n, 1, 2, 3)
+
+# boj 7576
+'''
+from collections import deque
+
+if __name__ == "__main__":
+    n, m = map(int, input().split(' '))
+    box = []
+    for _ in range(m):
+        box.append(list(map(int, input().split(' '))))
+
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+
+    qu = deque()
+
+    for i in range(m):
+        for j in range(n):
+            if box[i][j] <= 0:
+                continue
+            qu.append([i, j])
+
+    while qu:
+        cy, cx = qu.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if box[ny][nx] == 0:
+                box[ny][nx] = box[cy][cx] + 1
+                qu.append([ny, nx])
+
+    res = 0
+    for i in range(m):
+        for j in range(n):
+            if box[i][j] == 0:
+                print(-1)
+                exit(0)
+            res = max(res, box[i][j])
+    print(res - 1)
+'''
