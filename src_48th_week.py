232914b4a530e379848ae8a00ed349e04cc98d91
fabichoi <fabianus.c@gmail.com>
2020-11-23 22:32:34 +0900

    1d1p 2020.11.23

--- src_48th_week.py
+++ src_48th_week.py
@@ -1,1 +1,18 @@
 # 48th week
+
+# boj 2632
+
+if __name__ == "__main__":
+    s = int(input())
+    m, n = map(int, input().split(' '))
+
+    pa = []
+    pb = []
+
+    for _ in range(m):
+        pa.append(int(input()))
+    for _ in range(n):
+        pb.append(int(input()))
+
+
+
