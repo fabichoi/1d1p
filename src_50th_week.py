13fa1cdc4899095bd5602db90bffabdf1624227e
fabichoi <fabianus.c@gmail.com>
2020-12-07 23:20:02 +0900

    1d1p 2020.12.06

--- src_50th_week.py
+++ src_50th_week.py
@@ -1,1 +1,24 @@
 # 50th week
+
+# boj1173
+
+if __name__ == "__main__":
+    n, m, M, T, R = map(int, input().split(' '))
+    cnt = 0
+    now = m
+
+    if m + T > M:
+        print(-1)
+    else:
+        while n > 0:
+            if now + T <= M:
+                now += T
+                n -= 1
+            else:
+                if now - R >= m:
+                    now -= R
+                else:
+                    now = m
+
+            cnt += 1
+        print(cnt)
