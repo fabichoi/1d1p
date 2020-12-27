1e4374d702ca622d71fc3bd2a240f3c8b70f08c1
fabichoi <fabianus.c@gmail.com>
2020-12-28 08:36:39 +0900

    1d1p 2020.12.28

--- src_52nd_week.py
+++ src_52nd_week.py
@@ -1,1 +1,16 @@
 # 52nd week
+
+if __name__ == "__main__":
+    n = int(input())
+    mod_n = 9901
+
+    d = [0 for _ in range(n + 1)]
+    dc1 = [1 for _ in range(n + 1)]
+    dc2 = [0 for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        dc1[i] = (dc1[i - 1] + dc2[i - 1]) % mod_n
+        dc2[i] = (2 * dc1[i - 1] + dc2[i - 1]) % mod_n
+        d[i] = (dc1[i] + dc2[i]) % mod_n
+
+    print(d[n])
