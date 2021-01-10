64888db4fef676eb2e1dc339779aa4cc6ef98771
fabichoi <fabianus.c@gmail.com>
2021-01-11 08:35:19 +0900

    1d1p 2021.01.11

--- src_02nd_week.py
+++ src_02nd_week.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 '''
 
 # No.2
-
+'''
 if __name__ == "__main__":
     for _ in range(int(input())):
         n = int(input())
@@ -29,7 +29,28 @@ if __name__ == "__main__":
             print("NO")
         else:
             print("YES")
+'''
 
 
+# boj 11660
 
+from sys import stdin
 
+if __name__ == "__main__":
+    n, m = map(int, stdin.readline().split(' '))
+    ar = [[0 for _ in range(n+1)]]
+    for __ in range(n):
+        ar.append([0] + list(map(int, stdin.readline().split(' '))))
+
+    sar = [[0 for _ in range(n+1)] for __ in range(n+1)]
+    for y in range(1, n+1):
+        for x in range(1, n+1):
+            sar[y][x] = ar[y][x] + sar[y][x-1] + sar[y-1][x] - sar[y-1][x-1]
+
+    results = ''
+    for __ in range(m):
+        i, j, k, l = map(int, stdin.readline().split(' '))
+        result = sar[k][l] - sar[i-1][l] - sar[k][j-1] + sar[i-1][j-1]
+        results += str(result) + '\n'
+
+    print(results)
