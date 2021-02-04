b138745e15894875ae379ed5490cf7e42012c046
fabichoi <fabianus.c@gmail.com>
2021-02-04 23:09:24 +0900

    1d1p 2021.02.04

--- src_06th_week.py
+++ src_06th_week.py
@@ -1,3 +1,4 @@
+'''
 def solve(n):
     if int(n ** 0.5) ** 2 == n:
         return 1
@@ -11,3 +12,30 @@ def solve(n):
 
 
 print(solve(int(input())))
+'''
+
+'''
+for _ in range(int(input())):
+    cnt = 0
+    n = int(input())
+    while n > 1:
+        if n % 2 != 0:
+            cnt += 1
+        n /= 2
+    print("YES" if cnt > 1 else "NO")
+'''
+n = [2020 * i for i in range(501)]
+m = [2021 * i for i in range(501)]
+ar = []
+for i in range(501):
+    for j in range(501):
+        ar.append(n[i] + m[j])
+
+import sys
+res = ""
+for _ in range(int(sys.stdin.readline().rstrip())):
+    if int(sys.stdin.readline().rstrip()) in ar:
+        res += "YES\n"
+    else:
+        res += "NO\n"
+print(res)
