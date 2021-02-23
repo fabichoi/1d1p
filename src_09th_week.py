15ce5728c8c80ec903cca67f5c30a32042ddbca4
fabichoi <fabianus.c@gmail.com>
2021-02-23 20:05:24 +0900

    1d1p 2021.02.23

--- src_09th_week.py
+++ src_09th_week.py
@@ -1,2 +1,33 @@
 # python file for week of 9 in 2021
 
+'''
+def get_over_num(n, m):
+    a = int(n % m)
+    if a == 0:
+        return 0
+    return m - int(n % m)
+
+for _ in range(int(input())):
+    p, a, b, c = map(int, input().split(' '))
+    print(min(get_over_num(p, a), get_over_num(p, b), get_over_num(p, c)))
+'''
+
+'''
+n, m = map(int, input().split(' '))
+a, b = input(), input()
+
+m_cnt = 0
+idx = 0
+
+for i in range(m):
+    cnt = 0
+    for j in range(idx, n):
+        if b[i] == a[j]:
+            cnt += 1
+        else:
+            idx = j
+            break
+    m_cnt = max(m_cnt, cnt)
+
+print(m_cnt)
+'''
