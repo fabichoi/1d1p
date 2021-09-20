9639c779c49023e43aae7c9e3a3875e887d97157
fabichoi <fabianus.c@gmail.com>
2021-09-20 14:08:41 +0900

    1d1p 2021.09.20

--- 2021/38th/src_rod.py
+++ 2021/38th/src_rod.py
@@ -15,6 +15,26 @@ def solve(v, n):
     return price
 
 
+cache = [0 for _ in range(100000)]
+
+
+def solve_memo(v, n):
+    if n <= 0:
+        return 0
+    global cache
+
+    if cache[n - 1] != 0:
+        return cache[n - 1]
+
+    cache[n - 1] = -987654321
+
+    for i in range(1, n + 1):
+        cache[n - 1] = max(cache[n - 1], v[i] + solve_memo(v, n - i))
+
+    return cache[n - 1]
+
+
 if __name__ == '__main__':
     v = [0] + list(map(int, input().split()))
-    print(solve(v, len(v) - 1))
+    # print(solve(v, len(v) - 1))
+    print(solve_memo(v, len(v) - 1))
