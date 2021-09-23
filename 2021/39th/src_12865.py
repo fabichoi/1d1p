11dc47c2b5ea4294c06fb9c3d2b38d7e57c2fd3f
fabichoi <fabianus.c@gmail.com>
2021-09-23 20:53:41 +0900

    1d1p 2021.09.23

--- 2021/39th/src_12865.py
+++ 2021/39th/src_12865.py
@@ -1,17 +1,34 @@
 import sys
-sys.setrecursionlimit(10**5)
+
+sys.setrecursionlimit(10 ** 5)
+
 
 def solve(c, weight, value, n):
     if n <= 0 or c <= 0:
         return 0
 
-    if weight[n-1] > c:
-        return solve(c, weight, value, n-1)
+    if weight[n - 1] > c:
+        return solve(c, weight, value, n - 1)
 
-    carry = value[n-1] + solve(c - weight[n-1], weight, value, n-1)
-    leave = solve(c, weight, value, n-1)
+    carry = value[n - 1] + solve(c - weight[n - 1], weight, value, n - 1)
+    leave = solve(c, weight, value, n - 1)
 
     return max(carry, leave)
+
+
+def solve_dp(c, weight, value, n):
+    cache = [[0 for _ in range(c + 1)] for __ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, c + 1):
+            if weight[i - 1] <= j:
+                x = j - weight[i - 1]
+                cache[i][j] = max(value[i - 1] + cache[i - 1][x], cache[i - 1][j])
+            else:
+                cache[i][j] = cache[i - 1][j]
+
+    return cache[n][c]
+
 
 if __name__ == '__main__':
     n, c = map(int, input().split())
@@ -22,5 +39,4 @@ if __name__ == '__main__':
         weight.append(a)
         value.append(b)
 
-    print(solve(c, weight, value, n))
-
+    print(solve_dp(c, weight, value, n))
