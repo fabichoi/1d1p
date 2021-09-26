6af729d9d00c4cf60ba49336357c9fbf5c4aa2d0
fabichoi <fabianus.c@gmail.com>
2021-09-27 00:33:52 +0900

    1d1p 2021.09.27

--- 2021/39th/src_eggs.py
+++ 2021/39th/src_eggs.py
@@ -18,6 +18,26 @@ def solve(n, x):
     return init + 1
 
 
+def solve_dp(n, x):
+    cache = [[0 for _ in range(n + 1)] for __ in range(x + 1)]
+    for i in range(1, x + 1):
+        cache[i][1] = 1
+
+    for j in range(1, n + 1):
+        cache[1][j] = j
+
+    for i in range(2, x + 1):
+        for j in range(2, n + 1):
+            cache[i][j] = 987654321
+
+            for k in range(1, j + 1):
+                now = 1 + max(cache[i - 1][k - 1], cache[i][j - k])
+                if now < cache[i][j]:
+                    cache[i][j] = now
+
+    return cache[x][n]
+
+
 if __name__ == '__main__':
     n, x = map(int, input().split())
-    print(solve(n, x))
+    print(solve_dp(n, x))
