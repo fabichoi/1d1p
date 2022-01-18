a0c9e5e569eafa0dfe9e4f6cb9a90f649ec93842
fabichoi <fabianus.c@gmail.com>
2022-01-19 01:43:18 +0900

    1d1p 2022.01.18

--- 2022/04th/solve_220118.py
+++ 2022/04th/solve_220118.py
@@ -1,1 +1,17 @@
 # BOJ 1100
+res = 0
+for i in range(8):
+    ar = input()
+    if i % 2 == 0:
+        for j in range(8):
+            if j % 2 == 1:
+                continue
+            if ar[j] == 'F':
+                res += 1
+    else:
+        for j in range(8):
+            if j % 2 == 0:
+                continue
+            if ar[j] == 'F':
+                res += 1
+print(res)
