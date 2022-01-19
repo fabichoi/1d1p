dd43494aa29fdd44e4357f65413b91be798405a1
fabichoi <fabianus.c@gmail.com>
2022-01-19 23:47:33 +0900

    1d1p 2022.01.19

--- 2022/04th/solve_220119.py
+++ 2022/04th/solve_220119.py
@@ -1,1 +1,15 @@
 # BOJ 1110
+init_num = int(input())
+res = 0
+num = init_num
+
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = (b * 10) + c
+    res += 1
+    if init_num == num:
+        break
+
+print(res)
