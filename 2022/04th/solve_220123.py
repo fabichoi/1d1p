fe314e84a3f7443cea1c01e0a22bda8d2fe645c2
fabichoi <fabianus.c@gmail.com>
2022-01-23 15:02:02 +0900

    1d1p 2022.01.23

--- 2022/04th/solve_220123.py
+++ 2022/04th/solve_220123.py
@@ -1,1 +1,14 @@
 # BOJ 1159
+d = dict()
+for _ in range(int(input())):
+    i = input()[0]
+    if d.get(i) is None:
+        d[i] = 1
+        continue
+    d[i] += 1
+sorted_d = sorted(d.items())
+res = ''
+for r in sorted_d:
+    if r[1] >= 5:
+        res += r[0]
+print(res if len(res) > 0 else 'PREDAJA')
