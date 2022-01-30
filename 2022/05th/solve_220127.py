fdee00620821e32d3e9a3f235912420b77fa1fb8
fabichoi <fabianus.c@gmail.com>
2022-01-30 21:07:57 +0900

    1d1p 2022.01.30

--- 2022/05th/solve_220127.py
+++ 2022/05th/solve_220127.py
@@ -1,1 +1,11 @@
 # BOJ 1225
+from sys import stdin
+
+a, b = stdin.readline().split()
+s = 0
+aa = 0
+for i in range(len(a)):
+    aa += int(a[i])
+for i in range(len(b)):
+    s += aa * int(b[i])
+print(s)
--- 2022/05th/solve_220128.py
+++ 2022/05th/solve_220128.py
@@ -1,1 +1,12 @@
 # BOJ 1233
+from sys import stdin
+
+n, m, l = map(int, stdin.readline().split(' '))
+d = dict()
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        for k in range(1, l + 1):
+            s = i + j + k
+            d.setdefault(s, 0)
+            d[s] += 1
+print(sorted(d.items(), key=lambda x: (x[1], -x[0]), reverse=True)[0][0])
