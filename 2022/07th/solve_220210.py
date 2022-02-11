4eff779f272e8dd835adde6fb7a69b39ec7d25b0
fabichoi <fabianus.c@gmail.com>
2022-02-11 19:15:52 +0900

    solve 220210

--- 2022/07th/solve_220210.py
+++ 2022/07th/solve_220210.py
@@ -1,1 +1,16 @@
 # BOJ 1333
+def solve():
+    n, l, d = map(int, input().split(' '))
+    ar = []
+    music = [1 for _ in range(l)] + [0, 0, 0, 0, 0]
+    for i in range(1, n + 1):
+        ar += music
+    pos = 0
+    ar += [0 for _ in range(n+1)]
+    while pos < len(ar):
+        if ar[pos] == 0:
+            break
+        pos += d
+    return pos
+
+print(solve())
