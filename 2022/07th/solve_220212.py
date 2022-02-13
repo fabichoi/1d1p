5efa474d6f6986c5677c57db9470160de21e7fc4
fabichoi <fabianus.c@gmail.com>
2022-02-13 11:21:53 +0900

    solve 220212 and 220213

--- 2022/07th/solve_220212.py
+++ 2022/07th/solve_220212.py
@@ -1,1 +1,13 @@
-# BOJ 1356
+# BOJ 1350
+n = int(input())
+files = map(int, input().split(' '))
+cluster = int(input())
+res = 0
+for file in files:
+    if file == 0:
+        continue
+    m = file // cluster
+    if file % cluster > 0:
+        m += 1
+    res += cluster * m
+print(res)
