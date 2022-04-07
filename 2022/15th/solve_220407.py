774a83b5b0ec6df87512f4c47673799e97299c3f
fabichoi <fabianus.c@gmail.com>
2022-04-07 22:33:21 +0900

    1d1p 2022.04.07

--- 2022/15th/solve_220407.py
+++ 2022/15th/solve_220407.py
@@ -1,1 +1,21 @@
 # BOJ 1977
+from math import sqrt
+
+n = int(input())
+m = int(input())
+
+nn = int(sqrt(n))
+mm = int(sqrt(m))
+
+a, b = -1, 0
+for i in range(nn, mm+1):
+    k = i ** 2
+    if k < n or k > m:
+        continue
+    if a == -1:
+        a = k
+    b += k
+
+if a != -1:
+    print(b)
+print(a)
