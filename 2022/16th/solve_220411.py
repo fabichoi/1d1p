3f26da77cb1e09ba06a483d524d66f8f020e7cac
fabichoi <fabianus.c@gmail.com>
2022-04-12 00:04:08 +0900

    commit for 220411

--- 2022/16th/solve_220411.py
+++ 2022/16th/solve_220411.py
@@ -1,1 +1,13 @@
 # BOJ 2033
+n = input()
+res = ""
+last = 0
+for i in range(len(n) - 1, 0, -1):
+    nn = int(n[i]) + last
+    if nn >= 5:
+        last = 1
+    else:
+        last = 0
+    res = str(0) + res
+res = str(int(n[0]) + last) + res
+print(res)
