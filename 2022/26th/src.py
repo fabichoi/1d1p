4e41654dfbec6c02447034523e3ced446546240e
fabichoi <fabianus.c@gmail.com>
2022-06-25 21:00:21 +0900

    1d1p 2022.06.25

--- 2022/26th/src.py
+++ 2022/26th/src.py
@@ -1,3 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        rsorted = sorted(nums, reverse=True)
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != rsorted[i]:
+                count += 1
+            if count > 2:
+                break
+        return True if count == 2 else False
+
+
+s = Solution()
+print(s.checkPossibility(nums=[9, 3, 5]))
+
+'''
 import heapq
 from typing import List
 
@@ -27,3 +46,4 @@ class Solution:
 
 s = Solution()
 print(s.isPossible(target=[9, 3, 5]))
+'''
