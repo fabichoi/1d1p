35fe67f800c1c1e203585c0bc5ed939fcc52c0fc
fabichoi <fabianus.c@gmail.com>
2022-07-08 20:13:17 +0900

    :fire: 1d1p 2022.07.08

--- 2022/28th/src.py
+++ 2022/28th/src.py
@@ -1,3 +1,28 @@
+from functools import cache
+
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        @cache
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2):
+                return True
+            cs1, cs2 = False, False
+            if i < len(s1) and s1[i] == s3[i + j]:
+                cs1 = dfs(i + 1, j)
+            if j < len(s2) and s2[j] == s3[i + j]:
+                cs2 = dfs(i, j + 1)
+            return cs1 or cs2
+
+        return dfs(0, 0)
+
+
+s = Solution()
+print(s.isInterleave("aabc", "abad", "aabadabc"))
+'''
 from typing import List
 
 
@@ -17,3 +42,4 @@ class Solution:
 
 s = Solution()
 print(s.candy([1, 0, 2]))
+'''
