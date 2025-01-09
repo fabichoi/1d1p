d3fc5afbdc1ee0922862f7dd4afb533996961932
fabichoi <fabianus.c@gmail.com>
2025-01-09 22:13:43 +0900

    :fire: 1d1p 2025.01.09 add init training sources

---
+++ 2025/0109/mst.py
@@ -0,0 +1,41 @@
+def find(parent, i):
+    if parent[i] == i:
+        return i
+    return find(parent, parent[i])
+
+
+def union(parent, rank, x, y):
+    xroot = find(parent, x)
+    yroot = find(parent, y)
+
+    if rank[xroot] > rank[yroot]:
+        parent[yroot] = xroot
+        return
+    if rank[xroot] < rank[yroot]:
+        parent[xroot] = yroot
+        return
+
+    parent[yroot] = xroot
+    rank[xroot] += 1
+
+
+def solution(n, costs):
+    costs.sort(key=lambda x: x[2])
+    parent, rank = [i for i in range(n)], [0 for _ in range(n)]
+    min_cost, edges = 0, 0
+
+    for edge in costs:
+        if edges == n - 1:
+            break
+        x, y = find(parent, edge[0]), find(parent, edge[1])
+
+        if x != y:
+            union(parent, rank, x, y)
+            min_cost += edge[2]
+            edges += 1
+
+    return min_cost
+
+
+if __name__ == '__main__':
+    assert solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]) == 4
