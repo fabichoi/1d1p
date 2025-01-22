b2a162a188c1f6b5f93ead48e6cdfac6ed320484
fabichoi <fabianus.c@gmail.com>
2025-01-22 23:59:58 +0900

    :fire: 1d1p 2025.01.22 :muscle:

--- 2025/0122/dijkstra.py
+++ 2025/0122/dijkstra.py
@@ -1,6 +1,27 @@
 import heapq
 
-def solution
+
+def solution(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+    paths = {start: [start]}
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        if distances[current_node] < current_distance:
+            continue
+
+        for adjacent_node, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distances[adjacent_node] > distance:
+                distances[adjacent_node] = distance
+                paths[adjacent_node] = paths[current_node] + [adjacent_node]
+                heapq.heappush(queue, [distance, adjacent_node])
+
+    sorted_paths = {node: paths[node] for node in sorted(paths)}
+    return [distances, sorted_paths]
 
 
 if __name__ == '__main__':
