e2bb3d0751fbe671be27edfe11b7798d1c8da151
Fabichoi <fabianus.c@gmail.com>
2025-02-07 11:03:18 +0900

    :fire: 1d1p 2025.02.07 :muscle:

---
+++ 2025/0207/dfs.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+def solution(graph, start):
+    adj_list = defaultdict(list)
+    for u, v in graph:
+        adj_list[u].append(v)
+
+    def dfs(node, visited, result):
+        visited.add(node)
+        result.append(node)
+
+        for neighbor in adj_list.get(node, []):
+            if neighbor not in visited:
+                dfs(neighbor, visited, result)
+
+    visited, result = set(), list()
+    dfs(start, visited, result)
+    return result
+
+if __name__ == '__main__':
+    assert (solution([['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']], 'A')
+            == ['A', 'B', 'C', 'D', 'E'])
+    assert (solution([['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']], 'A')
+            == ['A', 'B', 'D', 'E', 'F', 'C'])
