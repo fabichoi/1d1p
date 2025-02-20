20156bd9358b554e023dec1007b597334297a0d1
Fabichoi <fabianus.c@gmail.com>
2025-02-20 20:51:19 +0900

    :fire: 1d1p 2025.02.20 :muscle:

--- 2025/0220/bellman-ford.py
+++ 2025/0220/bellman-ford.py
@@ -18,7 +18,6 @@ def solution(graph, source):
 
     return [distance, predecessor]
 
-
 if __name__ == '__main__':
     assert (solution([[(1, 4), (2, 3), (4, -6)], [(3, 5)], [(1, 2)], [(0, 7), (2, 4)], [(2, 2)]], 0)
             == [[0, -2, -4, 3, -6], [None, 2, 4, 1, 0]])
