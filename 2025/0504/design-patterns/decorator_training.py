1f86d0b5041b6649e99706178862e32dd3830d5e
fabichoi <fabianus.c@gmail.com>
2025-05-04 01:41:43 +0900

    :fire: 1d1p 2025.05.04 :muscle:

---
+++ 2025/0504/design-patterns/decorator_training.py
@@ -0,0 +1,34 @@
+# decorator pattern
+
+class WebRequest:
+    def handle_request(self):
+        return 'web request'
+
+
+class Decorator:
+    def __init__(self, req):
+        self.req = req
+
+    def handle_request(self):
+        return self.req.handle_request()
+
+
+class LoggingDecorator(Decorator):
+    def handle_request(self):
+        return "logging " + super().handle_request()
+
+
+class AuthenticationDecorator(Decorator):
+    def handle_request(self):
+        return "auth " + super().handle_request()
+
+
+request = WebRequest()
+logged_request = LoggingDecorator(request)
+auth_request = AuthenticationDecorator(request)
+logged_auth_request = LoggingDecorator(AuthenticationDecorator(request))
+
+assert request.handle_request() == "web request"
+assert logged_request.handle_request() == "logging web request"
+assert auth_request.handle_request() == "auth web request"
+assert logged_auth_request.handle_request() == "logging auth web request"
