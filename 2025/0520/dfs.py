8b7a0da02336c354ca9829c5287036c11d773151
Fabichoi <fabianus.c@gmail.com>
2025-05-20 22:08:33 +0900

    :fire: 1d1p 2025.05.20 :muscle:

---
+++ 2025/0520/dfs.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+def solution(graph, start):
+    adj_list = defaultdict(list)
+    for u, v in graph:
+        adj_list[u].append(v)
+
+
+
+if __name__ == '__main__':
+    assert (solution([['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']], 'A')
+            == ['A', 'B', 'C', 'D', 'E'])
+    assert (solution([['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']], 'A')
+            == ['A', 'B', 'D', 'E', 'F', 'C'])
