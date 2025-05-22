48d4220dbe2c657679131d9b058c4e6d0ce25a70
fabichoi <fabianus.c@gmail.com>
2025-05-22 17:21:06 +0900

    :fire: 1d1p 2025.05.22 :muscle:

---
+++ 2025/0522/design-patterns/strategy_training.py
@@ -0,0 +1,33 @@
+# strategy pattern
+
+class PaymentContext:
+    def __init__(self, payment):
+        self.payment = payment
+
+    def execute_payment(self, amount):
+        return self.payment.pay(amount)
+
+    def set_strategy(self, payment):
+        self.payment = payment
+
+
+class Payment:
+    def pay(self, amount):
+        raise NotImplementedError()
+
+
+class CreditCardPayment(Payment):
+    def pay(self, amount):
+        return f"cc: {amount}"
+
+
+class PayPalPayment(Payment):
+    def pay(self, amount):
+        return f"pp: {amount}"
+
+
+payment_context = PaymentContext(CreditCardPayment())
+assert payment_context.execute_payment(100) == "cc: 100"
+
+payment_context.set_strategy(PayPalPayment())
+assert payment_context.execute_payment(200) == "pp: 200"
